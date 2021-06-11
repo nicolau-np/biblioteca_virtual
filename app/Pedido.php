272f64fc7240e96c5ad40fd9a7b0b0dcec188add
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 20:04:55 +0100

    terminado modelo Pedido

--- app/Pedido.php
+++ app/Pedido.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    protected $table = "pedidos";
+
+    protected $fillable = [
+        'id_leitor',
+        'id_tipo_pedido',
+        'estado',
+    ];
+
+    public function leitor(){
+        return $this->belongsTo(Leitor::class, 'id_leitor', 'id');
+    }
+
+    public function tipo_pedido(){
+        return $this->belongsTo(TipoPedido::class, 'id_tipo_pedido', 'id');
+    }
 }
