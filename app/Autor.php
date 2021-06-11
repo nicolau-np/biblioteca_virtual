ecfa8c5c0b27b99a42171ecba0f62fbe1ddd50bb
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 19:51:51 +0100

    terminado modelo autor

--- app/Autor.php
+++ app/Autor.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Autor extends Model
 {
-    //
+    protected $table = "autors";
+
+    protected $fillable = [
+        'autor',
+        'estado',
+    ];
+
+    public function livro(){
+        return $this->hasMany(Livro::class, 'id_autor', 'id');
+    }
 }
