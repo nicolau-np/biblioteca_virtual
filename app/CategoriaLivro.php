3b3880abbd9fc517102ca62bd099457171ac8e22
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 19:48:17 +0100

    terminado modelo categoria livro

--- app/CategoriaLivro.php
+++ app/CategoriaLivro.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoriaLivro extends Model
 {
-    //
+    protected $table = "categoria_livros";
+
+    protected $fillable = [
+        'categoria',
+        'estado',
+    ];
+
+    public function livro(){
+        return $this->hasMany(Livro::class, 'id_categoria_livro', 'id');
+    }
 }
