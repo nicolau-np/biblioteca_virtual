71c3f3126cb0b3970147fed146fabdc99a7c3b76
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 19:23:55 +0100

    terminado migration livros

--- database/migrations/2021_06_11_175721_create_livros_table.php
+++ database/migrations/2021_06_11_175721_create_livros_table.php
@@ -14,8 +14,21 @@ class CreateLivrosTable extends Migration
     public function up()
     {
         Schema::create('livros', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_autor')->unsigned()->index();
+            $table->bigInteger('id_editora')->unsigned()->index();
+            $table->bigInteger('id_categoria_livro')->unsigned()->index();
+            $table->string('titulo');
+            $table->text('foto_capa')->nullable();
+            $table->date('data_lancamento')->nullable();
+            $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('livros', function (Blueprint $table) {
+            $table->foreign('id_autor')->references('id')->on('autors')->onUpdate('cascade');
+            $table->foreign('id_editora')->references('id')->on('editoras')->onUpdate('cascade');
+            $table->foreign('id_categoria_livro')->references('id')->on('categoria_livros')->onUpdate('cascade');
         });
     }
 
