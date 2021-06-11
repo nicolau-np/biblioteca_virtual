16cbf1cb3431e5641e330d17f3ac2401256d3ed5
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 19:50:07 +0100

    terminado modelo editora

--- app/Editora.php
+++ app/Editora.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Editora extends Model
 {
-    //
+    protected $table = "editoras";
+
+    protected $fillable = [
+        'editora',
+        'estado',
+    ];
+
+    public function livro(){
+        return $this->hasMany(Livro::class, 'id_editora', 'id');
+    }
 }
