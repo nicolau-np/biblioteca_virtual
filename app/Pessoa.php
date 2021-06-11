6b33802f85702b2e420624aa98a6d872012c8ffa
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 19:43:44 +0100

    terminado modelo pessoa

--- app/Pessoa.php
+++ app/Pessoa.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pessoa extends Model
 {
-    //
+    protected $table = "pessoas";
+
+    protected $fillable = [
+        'nome',
+        'genero',
+        'bi',
+        'foto',
+        'estado',
+    ];
+
+    public function usuario(){
+        return $this->hasMany(User::class, 'id_pessoa', 'id');
+    }
+
+    public function leitor(){
+        return $this->hasMany(Leitor::class, 'id_pessoa', 'id');
+    }
 }
