d8a4792577eeaac446b617e2ba938dc1c4cf1886
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-11 20:24:08 +0100

    adicionando novas rotas

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('leitores', "LeitorController");
+Route::group(['prefix'=>"leitors", ], function(){
+    Route::get('/', "LeitorController@index");
+    Route::get('/show/{id}', "LeitorController@show");
+    Route::post('/store', "LeitorController@store");
+    Route::put('/update/{id}', "LeitorController@update");
+    Route::delete('/destroy/{id}', "LeitorController@destroy");
+});
