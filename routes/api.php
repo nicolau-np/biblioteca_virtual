fb74342f45566b76acd35701d0c457e27247be5d
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-13 21:25:24 +0100

    criando rotas para os autores

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+
+Route::group(['prefix'=>"leitors", ], function(){
+    Route::get('/', "LeitorController@index");
+    Route::get('/show/{id}', "LeitorController@show");
+    Route::post('/store', "LeitorController@store");
+    Route::put('/update/{id}', "LeitorController@update");
+    Route::delete('/destroy/{id}', "LeitorController@destroy");
+});
 
 Route::group(['prefix'=>"leitors", ], function(){
     Route::get('/', "LeitorController@index");
