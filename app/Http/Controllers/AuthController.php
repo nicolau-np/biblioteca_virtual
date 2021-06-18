be93036389e0cc40d0fda3c6dd22cc46db64c1e2
Nicolau NP <mr1Normaliii@gmail.com>
2021-06-18 07:19:07 +0100

    terminando o controller Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,8 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         try {
             $users = User::with(['pessoa'])->get();
             return response()->json(['status' => "ok", 'data' => $users], 200);
@@ -20,17 +21,18 @@ class AuthController extends Controller
         }
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         try {
             $rules = [
                 'email' => ['required', 'email', 'unique:usuarios,email'],
-                'password'=>['required', 'string','min:6', 'max:100'],
-                'acesso' =>['required', 'string', 'min:3', 'max:255'],
+                'password' => ['required', 'string', 'min:6', 'max:100'],
+                'acesso' => ['required', 'string', 'min:3', 'max:255'],
                 'estadoU' => ['required', 'string', 'min:2'],
 
-                'nome'=> ['required', 'string', 'min:10', 'max:255',],
-                'genero' =>['required', 'string', 'min:1', 'max:255'],
-                'estadoP'=> ['required', 'string', 'min:2']
+                'nome' => ['required', 'string', 'min:10', 'max:255',],
+                'genero' => ['required', 'string', 'min:1', 'max:255'],
+                'estadoP' => ['required', 'string', 'min:2']
             ];
 
             $validator = Validator::make($request->all(), $rules);
@@ -39,24 +41,24 @@ class AuthController extends Controller
             }
             $password = Hash::make($request->password);
             $data['user'] = [
-                'id_pessoa'=>null,
-                'email'=>$request->email,
-                'password'=> $password,
-                'acesso'=> $request->acesso,
-                'estado'=> $request->estadoU,
+                'id_pessoa' => null,
+                'email' => $request->email,
+                'password' => $password,
+                'acesso' => $request->acesso,
+                'estado' => $request->estadoU,
             ];
             $data['pessoa'] = [
-                'nome'=>$request->nome,
-                'genero'=>$request->genero,
-                'bi'=> $request->bi,
-                'foto'=>null,
-                'estado'=> $request->estadoP,
+                'nome' => $request->nome,
+                'genero' => $request->genero,
+                'bi' => $request->bi,
+                'foto' => null,
+                'estado' => $request->estadoP,
             ];
 
             if ($request->hasFile('foto') && $request->foto->isValid()) {
 
                 $rules = [
-                    'foto'=>['required', 'mimes:jpg,jpeg,png,JPG,JPEG,PNG', 'max:10000']
+                    'foto' => ['required', 'mimes:jpg,jpeg,png,JPG,JPEG,PNG', 'max:10000']
                 ];
                 $validator = Validator::make($request->all(), $rules);
                 if ($validator->fails()) {
@@ -69,9 +71,9 @@ class AuthController extends Controller
 
 
             $pessoa = Pessoa::create($data['pessoa']);
-            if($pessoa){
-                $data['user']['id_pessoa']=$pessoa->id;
-                if(User::create($data['user'])){
+            if ($pessoa) {
+                $data['user']['id_pessoa'] = $pessoa->id;
+                if (User::create($data['user'])) {
                     return response()->json(['status' => "success", 'data' => "Feito com sucesso"], 200);
                 }
             }
@@ -82,54 +84,62 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $credentials = $request->only("username", "password");
-        $validator = Validator::make($credentials, [
-            'username' => 'required',
-            'password' => 'required',
-        ]);
-        if ($validator->fails()) {
+        try{
+            $rules = [
+                'email' => ['required', 'email'],
+                'password' => ['required', 'string', 'min:6'],
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json(['status' => 'validation', 'data' => $validator->errors()], 400);
+            }
+
+            if (!$token = JWTAuth::attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ])) {
+                return response()->json(['error' => 'Unauthorized', 'data'=>"Usuário não autoriazado"], 401);
+            }
             return response()->json([
-                'success' => false,
-                'errors' => $validator->errors()
-            ], 422);
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => JWTAuth::factory()->getTTL() * 60
+            ]);
+        }catch (\Exception $erro) {
+            return response()->json(['status' => "error", 'data' => $erro], 500);
         }
-        if (!$token = JWTAuth::attempt([
-            'username' => $request->username,
-            'password' => $request->password
-        ])
-        ) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => JWTAuth::factory()->getTTL() * 60
-        ]);
+
     }
 
     public function logout()
     {
-        JWTAuth::invalidate();
-        return response()->json([
-            'message' => 'Successfully logged out'
-        ]);
+        try{
+            JWTAuth::invalidate();
+            return response()->json(['status' => 'success', 'data'=>'Sessão terminada com sucesso']);
+        }catch(\Exception $erro){
+            return response()->json(['status' => "error", 'data' => $erro], 500);
+        }
+
     }
 
     public function refresh()
     {
-        return response()->json([
-            'access_token' => JWTAuth::refresh(),
-            'token_type' => 'bearer',
-            'expires_in' => JWTAuth::factory()->getTTL() * 60
-        ]);
+        try{
+            return response()->json(['status' =>'success','access_token' => JWTAuth::refresh(), 'token_type' => 'bearer', 'expires_in' => JWTAuth::factory()->getTTL() * 60]);
+        }catch(\Exception $erro){
+            return response()->json(['status' => "error", 'data' => $erro], 500);
+        }
+
     }
 
     public function user()
     {
-        return response()->json(
-            JWTAuth::user()
-        );
+        try{
+            return response()->json(JWTAuth::user());
+        }catch(\Exception $erro){
+            return response()->json(['status' => "error", 'data' => $erro], 500);
+        }
+
     }
-
-
 }
